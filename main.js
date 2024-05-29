edfc9d6414f73348550e7eea42f4f938edac9914
deek30 <134844597+deek30@users.noreply.github.com>
2024-05-29 11:36:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_8sh8Adb6Wi5o0NMKuGW1ci8Fg4znrGM",
+    authDomain: "portfolio-104d6.firebaseapp.com",
+    databaseURL: "https://portfolio-104d6-default-rtdb.firebaseio.com",
+    projectId: "portfolio-104d6",
+    storageBucket: "portfolio-104d6.appspot.com",
+    messagingSenderId: "755243343700",
+    appId: "1:755243343700:web:64480314e33c4cc7d0b2c1"
+  };
+  firebaseConfig.initializeApp(firebaseConfig);
+
+
+var typed = new Typed(".text", {
+    strings:["Frontend develper","Coder", "Web developer", "content writer..."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
